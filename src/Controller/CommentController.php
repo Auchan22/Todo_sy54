083646f085cb1c7d6aeb7d7d8862d8d440e4fd41
Agustin <agustinsurila@gmail.com>
2023-06-11 23:42:35 -0300

    Agregandole metodos a la ruta y validacion de parametros

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -1,19 +1,23 @@
 <?php
 
+
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CommentController extends AbstractController
 {
-    #[Route('/comment/{id}/vote/{direction}', name: 'app_comment', methods: ["POST"])]
-    public function commentVote($id, $direction): Response
+    #[Route('/comment/{id}/vote/{direction<up|down>}', name: 'app_comment', methods: ["POST"])]
+    public function commentVote(int $id, $direction, LoggerInterface $logger): Response
     {
         if($direction === "up"){
+            $logger->info("voting up");
             $currentVoteCount = rand(7, 100);
         }else{
+            $logger->info("voting down");
             $currentVoteCount = rand(0, 5);
         }
 
